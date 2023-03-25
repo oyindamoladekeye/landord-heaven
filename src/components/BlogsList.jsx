bf7e97e6eb3a4135f8f288cb1a7f76089251da8a
Adekeye Oyindamola <oyindamoladekeye@gmail.com>
2023-03-25 15:49:29 +0100

    mobile designs modifications

--- src/components/BlogsList.jsx
+++ src/components/BlogsList.jsx
@@ -4,7 +4,7 @@ import "../styles/blogslist.scss"
 export default function BlogsList({img , date, blogs}) {
   return (
     <div className='blogslist'>
-        <div><img src={img} alt="" /></div>
+        <div className='bloglist-img'><img src={img} alt="" /></div>
         <div className='bloglist-text'>
             <span>{date}</span>
             <p>{blogs}</p>

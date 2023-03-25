a33fbb5eeaba37d0baef11d46ef3e942adcd728d
Adekeye Oyindamola <oyindamoladekeye@gmail.com>
2023-03-25 18:41:04 +0100

    mobile design modifications

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import logo from "../assets/logo.png"
 import "../styles/navbar.scss"
+import {HiBars3BottomRight} from "react-icons/hi2"
 export default function Navbar() {
   return (
     <nav className='navbar'>
@@ -14,6 +15,8 @@ export default function Navbar() {
                 <li>Services</li>
                 <li>Explore</li>
             </ul>
+            {/* <HiBars3BottomRight  /> */}
+
         </div>
         <div className='btn'>
             <button className='button'>Login/Register</button>

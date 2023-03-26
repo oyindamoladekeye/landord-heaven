1863ff994d57fb0b89bd0596cedfa1c1d6d5b532
Adekeye Oyindamola <oyindamoladekeye@gmail.com>
2023-03-26 07:36:42 +0100

    mobile design modifications

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,24 +2,35 @@ import React from 'react'
 import logo from "../assets/logo.png"
 import "../styles/navbar.scss"
 import {HiBars3BottomRight} from "react-icons/hi2"
+import {RxCross2} from "react-icons/rx"
+import { useState } from 'react'
 export default function Navbar() {
+
+  const [showNavbar ,setShowNavbar] = useState(false);
+  const handleShowNavbar=()=>{
+    setShowNavbar(!showNavbar)
+  }
   return (
     <nav className='navbar'>
         <div>
             <img src={logo} alt="" />
         </div>
-        <div className='navbar-links'>
+        <div className={showNavbar?"nav-mobile-links":"nav-links"}>
             <ul>
                 <li>Home</li>
                 <li>Featured</li>
                 <li>Services</li>
                 <li>Explore</li>
             </ul>
-            {/* <HiBars3BottomRight  /> */}
-
+            <div className='btn1'>
+                <button className='button'>Login/Register</button>
+            </div>
         </div>
         <div className='btn'>
             <button className='button'>Login/Register</button>
+        </div>
+        <div className="toggle" onClick={handleShowNavbar}>
+          {showNavbar?<RxCross2 />:<HiBars3BottomRight />}
         </div>
     </nav>
   )
